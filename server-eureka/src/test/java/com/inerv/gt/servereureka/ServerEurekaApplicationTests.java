a47bb968f714bcb676bd6b9db81e38b46039504e
Ervin Emilio Canuz Marroquin <devecm4305@gmail.com>
2021-10-30 22:26:54 -0600

    Primer commit proyecto eureka server

---
+++ server-eureka/src/test/java/com/inerv/gt/servereureka/ServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inerv.gt.servereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
